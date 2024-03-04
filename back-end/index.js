3abe8dce9f3494937169d18596a2ba6f24c87392
leeap <Ssenge1515@gmail.com>
2024-03-04 20:24:59 +0800

    4-4

--- back-end/index.js
+++ back-end/index.js
@@ -3,14 +3,13 @@ import { sql } from "./config/database.js";
 const app = express();
 const port = 3000;
 
-app.get("/users", async (req, res) => {
+app.get("/users/get", async (req, res) => {
   const data = await sql`SELECT *FROM users`;
   console.log(data);
   res.send("hello");
 });
-app.post("/users", async (req, res) => {
-  const data =
-    await sql`INSERT INTO users(name ,email) VALUES('s','s') RETURNING *`;
+app.put("/users", async (req, res) => {
+  const data = await sql`SELECT *FROM users`;
   console.log(data);
   res.send("hello");
 });
