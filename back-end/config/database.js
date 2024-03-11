574eeca1a0c202084a3f308883a88e9acca2260e
leeap <Ssenge1515@gmail.com>
2024-03-11 19:21:56 +0800

    connected neon

--- back-end/config/database.js
+++ back-end/config/database.js
@@ -1,21 +1,19 @@
-// ./config/database.js
 import postgres from "postgres";
 import dotenv from "dotenv";
 
 dotenv.config();
 
+console.log(process.env.PGDATABASE);
 let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
 
-const sql = postgres({
-  host: PGHOST,
-  database: PGDATABASE,
-  username: PGUSER,
-  password: PGPASSWORD,
-  port: 5432,
-  ssl: "require",
-  connection: {
-    options: `project=${ENDPOINT_ID}`,
-  },
-});
-
-export { sql };
+// const sql = postgres({
+//   host: PGHOST,
+//   database: PGDATABASE,
+//   username: PGUSER,
+//   password: PGPASSWORD,
+//   port: 5432,
+//   ssl: "require",
+//   connection: {
+//     options: `project=${ENDPOINT_ID}`,
+//   },
+// });
--- back-end/index.js
+++ back-end/index.js
@@ -4,6 +4,12 @@ import { sql } from "./config/database.js";
 const app = express();
 const port = 3000;
 
+app.get("/category", async (req, res) => {
+  const data = await sql`SELECT *FROM users`;
+  console.log(data);
+  res.send("hello");
+});
+
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
 });
