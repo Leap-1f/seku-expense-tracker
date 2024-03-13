7290aceb34794bc458ea9a4b5ffd7f22df322cf6
leeap <Ssenge1515@gmail.com>
2024-03-13 19:16:02 +0800

    3-13

--- back-end/config/database.js
+++ back-end/config/database.js
@@ -3,17 +3,16 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-console.log(process.env.PGDATABASE);
 let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
 
-// const sql = postgres({
-//   host: PGHOST,
-//   database: PGDATABASE,
-//   username: PGUSER,
-//   password: PGPASSWORD,
-//   port: 5432,
-//   ssl: "require",
-//   connection: {
-//     options: `project=${ENDPOINT_ID}`,
-//   },
-// });
+export const sql = postgres({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: "require",
+  connection: {
+    options: `project=${ENDPOINT_ID}`,
+  },
+});
--- back-end/index.js
+++ back-end/index.js
@@ -1,28 +1,12 @@
-// app.js or server.js
-
 import express from "express";
-import dotenv from "dotenv";
-import signupRouter from "./src/router/users.js";
-
-dotenv.config();
+import { sql } from "./config/database.js";
 
 const app = express();
+const PORT = 8080;
 
-<<<<<<< HEAD
-// Middleware to parse JSON bodies
-app.use(express.json());
-=======
-app.get("/category", async (req, res) => {
-  const data = await sql`SELECT *FROM users`;
-  console.log(data);
-  res.send("hello");
+app.get("/users", async (request, response) => {
+  const Userdata = await sql`SELECT * FROM users`;
 });
->>>>>>> 574eeca1a0c202084a3f308883a88e9acca2260e
-
-// Mount the signup router
-app.use("/api", signupRouter);
-
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
